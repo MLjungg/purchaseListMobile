fdd683179636b89961dde5dc20167c107ecb7456
milj <mikael.lj@hotmail.com>
2020-08-30 17:00:44 +0200

    Added playground code for learning purpose

--- PurchaseList/purchaseList/ViewController.swift
+++ PurchaseList/purchaseList/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        nameTextField.text = ""
+    }
 }
 
